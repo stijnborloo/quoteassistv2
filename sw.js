e4a81d611997de6f39b2dbd791598e00dc575f80
stijnborloo <stijn.borloo@ricoh.be>
2026-08-29 17:54:30 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // QuoteStudio - Service Worker
 // Timestamp: 2026-06-22T00:00:00Z
-var CACHE = "quotestudio-v11";
+var CACHE = "visioffer-v9";
 var INDEX = "index.html";
 
 self.addEventListener("install", function(e) {
